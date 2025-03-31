f35b5faae76ed70431b1e7fa33f09be32b9f236b
Ansh Joshi <ansh.joshi2608@gmail.com>
2025-03-31 15:17:23 +0100

    files added

---
+++ netflix/src/utils/constant.js
@@ -0,0 +1,19 @@
+export const API_END_POINT = "http://localhost:8080/api/v1/user";
+
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjM2NhZGRkZjQ1MzMwYTY1NWMyM2VmMTg5MDY3NmMwMCIsIm5iZiI6MTczOTYyNzYwMS45MDQsInN1YiI6IjY3YjA5YzUxYWY5YjBjYWFlMTM1YjNiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gsq-MVpyNPLWhH8zTjncuapg-WNFkMc6ypOj7mRmEL8'
+    }
+  };
+
+  export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+  export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+  export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+  export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+
+  export const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie?query=";
+
+  export const TMDB_IMG_URL ="https://image.tmdb.org/t/p/w500";
